01cbd47d16eefdacbf46ca92eb3a808549a38200
p4t1m4 <prati.ma@hotmail.com>
2022-06-22 11:41:34 +0200

    component header is complete

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -9,9 +9,11 @@ export const Header = () => {
     { label: "Contact", href: "/" },
   ];
   return (
-    <header className=" bg-oxblood grid grid-cols-2 p-3">
-      <Logo />
-      <Menu items={menuItems} />
+    <header className="  bg-oxblood p-3">
+      <div className="container mx-auto grid grid-cols-4 gap-3 ">
+        <Logo />
+        <Menu items={menuItems} />
+      </div>
     </header>
   );
 };
