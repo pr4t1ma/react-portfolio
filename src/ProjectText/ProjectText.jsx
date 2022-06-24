ad72f57d70cb8c1781da376a98509cd7d83fa5d7
p4t1m4 <prati.ma@hotmail.com>
2022-06-24 15:24:58 +0200

    responsiv design for the small screen

--- src/ProjectText/ProjectText.jsx
+++ src/ProjectText/ProjectText.jsx
@@ -10,13 +10,13 @@ export const ProjectText = ({ title, subtitle, text }) => {
     { label: "React", href: "/" },
   ];
   return (
-    <div className=" md:col-start-5 md:col-end-12 flex-1 row-start-1 row-end-2  ">
-      <div className="project-title text-oxblood font-bold text-right pb-5 ">
+    <div className=" md:col-start-5 md:col-end-12 md:flex-1 md:row-start-1 md:row-end-2  ">
+      <div className="project-title text-oxblood font-bold text-right mb-5 ">
         <h1 className="text-2xl text-black ">{title}</h1>
         <h2 className="text-xl py-3 flex-end">{subtitle}</h2>
       </div>
 
-      <p className="bg-forestgreen text-white px-5  py-8  shadow-2xl shadow-gray">
+      <p className="bg-forestgreen text-white px-5 mb-10  py-8  shadow-2xl shadow-gray">
         {" "}
         {text}
       </p>
