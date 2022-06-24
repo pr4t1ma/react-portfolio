e043f49dec0156291cb8c6c63c3a2098be0a7324
p4t1m4 <prati.ma@hotmail.com>
2022-06-24 15:24:24 +0200

    add footer component

--- src/Footer/Footer.jsx
+++ src/Footer/Footer.jsx
@@ -1,3 +1,17 @@
+import { FooterMenu } from "../FooterMenu/FooterMenu";
+
 export const Footer = () => {
-  return <h1 className="text-3xl">Hello welcome to my site</h1>;
+  const footermenu = [
+    { label: "home", href: "/" },
+    { label: "home", href: "/" },
+    { label: "home", href: "/" },
+    { label: "home", href: "/" },
+  ];
+  return (
+    <div className="container mx-auto bg-oxblood p-10 text-center shadow-4xl shadow-red-800 text-white">
+      <h1 className="text-xl ">Hello welcome to my site</h1>
+      <FooterMenu items={footermenu} />
+      <p>@copyright 2022</p>
+    </div>
+  );
 };
