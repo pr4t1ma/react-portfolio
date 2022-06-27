c38e12db119cd7e07f22bc87419bd4788ca6f82e
p4t1m4 <prati.ma@hotmail.com>
2022-06-27 13:41:17 +0200

    add bunch of project in a projects div

--- src/Project/Project.jsx
+++ src/Project/Project.jsx
@@ -1,14 +1,26 @@
-export const Project = ({ link, text }) => {
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import { faFolder, faGlobe } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+export const Project = ({ link, text, title }) => {
   return (
     <div className="container mx-auto p-8 bg-white shadow-2xl shadow-gray my-20">
       <div className="project-header flex justify-between">
         <div className="project-folder ">
-          <a href="#"> this is a link too</a>
+          <a className=" text-2xl text-oxblood" href="#">
+            <FontAwesomeIcon icon={faFolder} />
+          </a>
         </div>
-        <div className="polyline">
-          <a href="#"> this is a link</a>
-          <a href="#">this is a second column second link</a>
+        <div className="polyline text-2xl text-oxblood flex gap-5">
+          <a href="#">
+            <FontAwesomeIcon icon={faGlobe} />
+          </a>
+          <a href="#">
+            <FontAwesomeIcon icon={faGithub} />
+          </a>
         </div>
+      </div>
+      <div className="project-title">
+        <h3>{title}</h3>
       </div>
       <div className="project-description">
         <p>
