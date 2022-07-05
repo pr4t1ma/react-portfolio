517e09f28d78f9209c9cd6e2a2b899cf13116db9
p4t1m4 <prati.ma@hotmail.com>
2022-07-05 12:17:21 +0200

    todo Itme completed

---
+++ src/stateMessage/TodoItem.jsx
@@ -0,0 +1,22 @@
+import { useState } from "react";
+
+export const TodoItem = ({ id, text, onDelete }) => {
+  const [done, setDone] = useState(false);
+  return (
+    <div className={`flex my-3 ${done ? "line-through" : ""}`}>
+      <input
+        className="my-2 mr-5"
+        type="checkbox"
+        defaultChecked={done}
+        onChange={(e) => {
+          setDone(!done);
+          console.log(done);
+        }}
+      />{" "}
+      {text}
+      <button className="ml-10" onClick={() => onDelete(id)}>
+        x
+      </button>
+    </div>
+  );
+};
