f2f4005c809441d13a36a2e0f11e88f83d9d5a6b
p4t1m4 <prati.ma@hotmail.com>
2022-07-12 18:04:52 +0200

    complete new component Accordion

---
+++ src/App.jsx
@@ -0,0 +1,60 @@
+import "./App.css";
+
+import { BannerText } from "./BannerText/BannerText";
+import { Footer } from "./Footer/Footer";
+import { Header } from "./Header/Header";
+import { Heading } from "./Heading/Heading";
+import { Intro } from "./Intro/Intro";
+import { Project } from "./Project/Project";
+import { FeaturedProject } from "./FeaturedProject/FeaturedProject";
+import { TabbedPanels } from "./TabbedPanels/TabbedPanels";
+import { GetInTouch } from "./GetInTouch/GetInTouch";
+import { TodoList } from "./TodoList/TodoList";
+import { TestTodoList } from "./SimpleProject/PracticeTodolist/TestTodoList";
+import { Calculator } from "./Calculator/Calculator";
+import { Accordion } from "./TodoList/Accordion/Accordion";
+import { accordionData } from "./Data/AccordionData";
+import { TabPanel } from "./TabPanel/TabPanel";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Intro />
+      <Heading text="About me" />
+      <BannerText />
+      <Heading text="My Experence" />
+      <TabbedPanels />
+      <Heading text="My featured Projects" />
+      <FeaturedProject />
+      <div className="Projects container  mx-auto grid gap-3 grid-cols-1  my-5 md:grid md:grid-cols-2 lg:grid lg:grid-cols-3 lg:my-10">
+        <Project />
+        <Project />
+        <Project />
+      </div>
+      <div className="container mx-auto place-items-center w-fit block mb-20 shadow-2xl shadow-gray">
+        <a className=" button">See more</a>
+      </div>
+      <GetInTouch
+        title="whats next"
+        subtitle="Get in touch"
+        text="loremipsum the most be more text about personal information hobbies and other topic most be here written  "
+      />
+      <TodoList />
+
+      <TestTodoList />
+      <Calculator />
+      <div className="container mx-auto">
+        <h1 className="text-center text-oxblood font-bold">Accordion</h1>
+        {accordionData.map(({ title, content }) => (
+          <Accordion title={title} content={content} />
+        ))}
+      </div>
+      <TabPanel />
+
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
