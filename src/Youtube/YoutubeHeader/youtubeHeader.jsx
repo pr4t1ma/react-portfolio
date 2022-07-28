bc3c051c368612c3d7d3ccb748dc55f0cc2ba918
p4t1m4 <prati.ma@hotmail.com>
2022-07-28 11:34:25 +0200

    Youtube header menu

--- src/Youtube/YoutubeHeader/youtubeHeader.jsx
+++ src/Youtube/YoutubeHeader/youtubeHeader.jsx
@@ -3,11 +3,12 @@ import { YoutubeMenu } from "../YoutubeMenu/YoutubeMenu";
 
 export const YoutubeHeader = () => {
   const items = [
-    { label: "Home" },
-    { label: "About" },
-    { label: "Blog" },
-    { label: "Contact" },
+    { label: "Home", href: "./" },
+    { label: "About", href: "/about" },
+    { label: "Blog", href: "/blog" },
+    { label: "Contact", href: "/contact" },
   ];
+
   return (
     <div className="container mx-auto flex justify-between">
       <YoutubeMenu items={items} />
--- src/Youtube/YoutubeMenu/YoutubeMenu.jsx
+++ src/Youtube/YoutubeMenu/YoutubeMenu.jsx
@@ -1,12 +1,40 @@
+import { faBars, faMultiply } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
 export const YoutubeMenu = ({ items }) => {
+  const [isMenuOpen, setIsMenuOpen] = useState(true);
+
+  function openMenu() {
+    setIsMenuOpen(true);
+  }
+  function menuClose() {
+    setIsMenuOpen(false);
+  }
   return (
     <div className="container mx-auto my-7">
-      <ul className="flex gap-4 ">
-        {items.map((item) => (
-          <li key={item.label} className="hover:border-b-2 p-1">
-            {item.label}
-          </li>
-        ))}
+      <div className="font-icon" onClick={openMenu}>
+        <FontAwesomeIcon icon={faBars} />
+      </div>
+
+      <ul className="sm:flex gap-4 ">
+        <div className="font-ico" onClick={menuClose}>
+          <FontAwesomeIcon className="text-2xl" icon={faMultiply} />
+        </div>
+        <div>
+          {isMenuOpen ? (
+            <FontAwesomeIcon icon={faMultiply} />
+          ) : (
+            <FontAwesomeIcon icon={faBars} />
+          )}
+        </div>
+        {isMenuOpen &&
+          items.map((item) => (
+            <li key={item.label} className="hover:border-b-2 p-1">
+              <Link to={item.href}>{item.label}</Link>
+            </li>
+          ))}
       </ul>
     </div>
   );
