a0a854ca4a3fe8f0326087a83fc2cb834f7cc172
Pam <prati.ma@hotmail.com>
2024-10-31 07:24:10 +0100

    All change

--- src/Tabs/Tabs.jsx
+++ src/Tabs/Tabs.jsx
@@ -12,16 +12,17 @@ export const Tabs = ({ items, onTabChange }) => {
   return (
     <ul className=" border-l-2 border-oxblood border-solid ">
       {items.map((item, i) => (
-        <li
-          key={item.label}
-          className={`${
-            active === i ? "active" : ""
-          } py-3 px-4 hover:bg-oxblood hover:text-white`}
-          onClick={() => {
-            onItemClick(i);
-          }}
-        >
-          {item.label}
+        <li key={item.label}>
+          <button
+            className={`${
+              active === i ? "active" : ""
+            } w-full block py-3 px-4 hover:bg-oxblood hover:text-white`}
+            onClick={() => {
+              onItemClick(i);
+            }}
+          >
+            {item.label}
+          </button>
         </li>
       ))}
     </ul>
